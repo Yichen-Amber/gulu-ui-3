19817c90a273ffc2b99e43699a29655f9bcd96d3
Yichen <2283815345@qq.com>
2021-11-19 16:43:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div> hi</div>
+ <Amber/>
+</template>
+
+<script>
+import Amber from './components/Amber.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Amber
+  }
+}
+</script>
