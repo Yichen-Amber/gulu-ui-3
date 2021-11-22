2835c3533b4e5e21d671e600b0b492fe1789371c
Yichen <2283815345@qq.com>
2021-11-22 17:21:07 +0800

    button组件属性绑定

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-  Button 示例
-  </div>
+  <div>Button 示例</div>
   <h1>示例1</h1>
-  <button>你好</button>
+  <div>
+    <Button @click="onClick"
+      @focus="onClick"
+      @mouseover="onClick"
+    >你好</Button>
+  </div>
 </template> 
-</template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
+  setup(){
+      const onClick=()=>{
+          console.log('hi')
+      }
+      return{onClick}
+  }
 };
 </script> 
