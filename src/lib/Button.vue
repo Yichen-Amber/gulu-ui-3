79d651bba3fbb18e79386a50d738a08c0d417cc1
Yichen <2283815345@qq.com>
2021-11-25 14:18:37 +0800

    创建 Dialog 组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="gulu-button":class="classes" :disabled="disabled">
+  <button class="gulu-button" :class="classes" :disabled="disabled">
      <span v-if="loading" class="gulu-loadingIndicator"></span>
       <slot />
     </button>
