a2069cff5dabac3c07cc85baa7c7c7e17bda3ee3
Yichen <2283815345@qq.com>
2021-12-09 15:45:33 +0800

    忽略dist目录

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import { md } from "./plugins/md";
 import fs from 'fs'
 import { baseParse } from '@vue/compiler-core'
-import { marked } from 'marked';
+import marked from 'marked'
 
 export default {
     plugins: [md()],
